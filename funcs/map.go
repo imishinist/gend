62519991ede72cfb5b06ae0755611edb5e634442
Taisuke Miyazaki <imishinist@gmail.com>
2021-04-23 13:19:04 +0900

    add trimjoin

--- funcs/map.go
+++ funcs/map.go
@@ -6,5 +6,15 @@ import (
 )
 
 var Map = template.FuncMap{
-	"join": strings.Join,
+	"join":     strings.Join,
+	"trimjoin": trimJoin,
+	"trim":     strings.TrimSpace,
+}
+
+func trimJoin(elems []string, sep string) string {
+	trimed := make([]string, 0, len(elems))
+	for _, e := range elems {
+		trimed = append(trimed, strings.TrimSpace(e))
+	}
+	return strings.Join(trimed, sep)
 }
